1e8ecf642f835408147a4d3a1ff777bd060d1f9d
krzysztofstefanski <krzysztof.stefanski82@gmail.com>
2019-01-05 22:47:48 +0100

    add state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
+import Todos from './components/Todos'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>App</h1>
-       
+
+  state = {
+    todos : [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+
+      }
+    ]
+  }
+render() {
+  console.log(this.state.todos[2].title)
+    return ( 
+      <div className = "App" >
+      
+      <Todos / >
+
+      
       </div>
     )
   }
